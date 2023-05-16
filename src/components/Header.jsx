cda8584299696e77dbff03f8e5ff0bad3c5c75d2
bakhacode <bakhamitai@naver.com>
2023-05-16 15:43:00 +0900

    style: 서치바 색깔 변경, 장르태그 색깔 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,13 @@ import { SearchBar } from './SearchBar';
 
 export function Header() {
   return (
-    <header className=" flex flex-col justify-center items-center">
-      <div className="text-[50px]">GENRE WIKI</div>
-      <SearchBar />
+    <header className="flex flex-col">
+      {/* <div className="h-[100px] leading-[100px] text-center text-[50px]"> */}
+      <div className="mx-auto m-[20px] text-[50px] text-[black]]">GENRE WIKI</div>
+      {/* </div> */}
+      <div className="mx-auto mt-[10px] flex items-center">
+        <SearchBar />
+      </div>
     </header>
   );
 }
