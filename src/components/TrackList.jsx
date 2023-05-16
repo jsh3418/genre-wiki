f65d0a649f32a6484bc49ff204f25791a680fb95
bakhacode <bakhamitai@naver.com>
2023-05-16 16:03:03 +0900

    style: 서치바 색깔 변경, 장르태그 색깔 변경

--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -1,7 +1,7 @@
 export function TrackList({ data }) {
   return (
-    <div className="mt-[10px] relative flex justify-center items-center top-[40px]">
-      <ul className="grid gap-[30px]">
+    <div className="mt-[10px] mx-auto relative flex justify-center items-center top-[40px]">
+      <ul className="grid gap-[30px] mx-auto">
         {data.map((track, index) => (
           <li className="flex rounded-[8px] shadow-[0_4px_24px_rgba(48,62,75,.06)]" key={index}>
             <img className="w-[150px] h-[150px]" src={track.image} alt={track.name} />
@@ -9,21 +9,40 @@ export function TrackList({ data }) {
               <div className="text-[14px]">{track.artist}</div>
               <div className="text-[18px]">{track.name}</div>
             </div>
-            <div className="w-[400px] flex justify-center items-center gap-[15px]">
-              <div className="flex font-[200] justify-center text-[15px]  w-[80px] border-[1px] rounded-[25px] border-[#243c5a]">
-                pop
+            <div className="px-[20px] flex flex-col justify-evenly ">
+              <div className="flex justify-center items-center gap-[15px]">
+                <div className="flex font-[700] bg-[#FFFF64] justify-center text-[15px]  w-[80px] border-[1px] rounded-[25px] border-[#243c5a]">
+                  pop
+                </div>
+                <div className="flex font-[700] bg-[#FFFF64] justify-center text-[15px]  w-[80px] border-[1px] rounded-[25px] border-[#243c5a]">
+                  rock
+                </div>
+                <div className="flex font-[700] bg-[#FFFF64] justify-center text-[15px]  w-[80px] border-[1px] rounded-[25px] border-[#243c5a]">
+                  hiphop
+                </div>
+                <div className="flex font-[700] bg-[#FFFF64] justify-center text-[15px]  w-[80px] border-[1px] rounded-[25px] border-[#243c5a]">
+                  dance
+                </div>
+                <div className="flex font-[700] bg-[#FFFF64] justify-center text-[15px]  w-[80px] border-[1px] rounded-[25px] border-[#243c5a]">
+                  house
+                </div>
               </div>
-              <div className="flex font-[200] justify-center text-[15px]  w-[80px] border-[1px] rounded-[25px] border-[#243c5a]">
-                rock
-              </div>
-              <div className="flex font-[200] justify-center text-[15px]  w-[80px] border-[1px] rounded-[25px] border-[#243c5a]">
-                hiphop
-              </div>
-              <div className="flex font-[200] justify-center text-[15px]  w-[80px] border-[1px] rounded-[25px] border-[#243c5a]">
-                dance
-              </div>
-              <div className="flex font-[200] justify-center text-[15px]  w-[80px] border-[1px] rounded-[25px] border-[#243c5a]">
-                house
+              <div className="flex justify-center items-center gap-[15px]">
+                <div className="flex font-[200] justify-center text-[15px]  w-[80px] border-[1px] rounded-[25px] border-[#243c5a]">
+                  user
+                </div>
+                <div className="flex font-[200] justify-center text-[15px]  w-[80px] border-[1px] rounded-[25px] border-[#243c5a]">
+                  user
+                </div>
+                <div className="flex font-[200] justify-center text-[15px]  w-[80px] border-[1px] rounded-[25px] border-[#243c5a]">
+                  user
+                </div>
+                <div className="flex font-[200] justify-center text-[15px]  w-[80px] border-[1px] rounded-[25px] border-[#243c5a]">
+                  user
+                </div>
+                <div className="flex font-[200] justify-center text-[15px]  w-[80px] border-[1px] rounded-[25px] border-[#243c5a]">
+                  user
+                </div>
               </div>
             </div>
           </li>
